4fc7fec3a35db76357d0dee9f81895242166d407
Nonton Anime <artdengun@gmail.com>
2021-03-31 17:51:15 +0700

    menambahkan report pdf dan excel untuk class Anggota

--- src/main/java/com/deni/gunawan/Sisteminformasiperpustakaan/controller/AnggotaController.java
+++ src/main/java/com/deni/gunawan/Sisteminformasiperpustakaan/controller/AnggotaController.java
@@ -1,11 +1,18 @@
 package com.deni.gunawan.Sisteminformasiperpustakaan.controller;
 
 
+import com.deni.gunawan.Sisteminformasiperpustakaan.configuration.AnggotaExcelExporter;
 import com.deni.gunawan.Sisteminformasiperpustakaan.model.Anggota;
-import com.deni.gunawan.Sisteminformasiperpustakaan.model.Buku;
+import com.deni.gunawan.Sisteminformasiperpustakaan.model.Anggota;
 import com.deni.gunawan.Sisteminformasiperpustakaan.service.AnggotaService;
 
+import com.deni.gunawan.Sisteminformasiperpustakaan.service.AnggotaService;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +20,21 @@ import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import java.util.List;
-import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Controller
 public class AnggotaController {
 
     @Autowired  private AnggotaService anggotaService;
+    @Autowired
+    ApplicationContext context;
+
 
     @GetMapping("anggota")
     public String getAnggota(Model model){
@@ -51,6 +66,49 @@ public class AnggotaController {
     public String delete(String id){
         anggotaService.delete(id);
         return "redirect:/anggota";
+    }
+
+    @GetMapping(path = "anggota/report/pdf/LaporanAnggota")
+    @ResponseBody
+    public void getPdf(HttpServletResponse response) throws Exception {
+        Resource resource = context.getResource("classpath:reports/anggota.jrxml");
+
+        InputStream inputStream = resource.getInputStream();
+        JasperReport report = JasperCompileManager.compileReport(inputStream);
+        //Parameters Set
+        Map<String, Object> params = new HashMap<>();
+
+        List<Anggota> anggotas = (List<Anggota>) anggotaService.getAnggotaList();
+
+        //Data source Set
+        JRDataSource dataSource = new JRBeanCollectionDataSource(anggotas);
+        params.put("datasource", dataSource);
+
+        //Make jasperPrint
+        JasperPrint jasperPrint = JasperFillManager.fillReport(report, params, dataSource);
+        //Media Type
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        //Export PDF Stream
+        JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
+    }
+
+
+    @GetMapping("anggota/report/excel/LaporanAnggota")
+    public void exportToExcel(HttpServletResponse response) throws IOException {
+        response.setContentType("application/octet-stream");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=laporanAnggota" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+
+        List<Anggota> listAnggota = anggotaService.getAnggotaList();
+
+        AnggotaExcelExporter anggotaExcelExporter = new AnggotaExcelExporter(listAnggota);
+
+        anggotaExcelExporter.export(response);
+
     }
 
 
